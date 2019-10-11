cfcccdd8651eeb9b0b2d477376bfc5a0abce6c9e
Aditya <aditya10699@gmail.com>
2019-10-12 00:13:11 +0530

    enable cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const cors = require('cors');
 app.use(cookieParser());
 // Use heroku env variable or port 7000
 const port = process.env.PORT || 7000;
@@ -16,16 +17,17 @@ app.use(session({
     saveUninitialized: true
 }));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 // body parser middleware
 app.use(express.json());
 // url encoding
 app.use(express.urlencoded({extended:false}));
-    
+
+app.use(cors());
 // Routes
 app.use('/',require('./server'));
