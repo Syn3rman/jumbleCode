3b9a0232ef6291a2aac03b35b4833f95597201b5
Aditya <aditya10699@gmail.com>
2019-10-11 20:37:35 +0530

    switch to mlab

--- server/dbconnection.js
+++ server/dbconnection.js
@@ -1,12 +1,13 @@
- 
 const MongoClient = require('mongodb').MongoClient;
+const { password } = require('../secrets.js');
 
-const mongourl='mongodb://localhost:27017/';
+
+const mongourl=`mongodb://codecell:${password}@ds333768.mlab.com:33768/crackathon`;
 console.log(mongourl);
 
 // connecting to db server
 async function get(){
-  let db = await MongoClient.connect(mongourl, {useNewUrlParser: true, useUnifiedTopology: true});
+  let db = await MongoClient.connect(mongourl, {useNewUrlParser: true, useUnifiedTopology: true}).catch(e=>console.log(e));
   return db;
 }
 
