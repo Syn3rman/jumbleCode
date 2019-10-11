5c23aad8436d38673d711cdce4e123c72b99d4ce
Aditya <aditya10699@gmail.com>
2019-10-11 23:33:13 +0530

    Renamed app to server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+app.use(cookieParser());
+// Use heroku env variable or port 7000
+const port = process.env.PORT || 7000;
+app.listen(port,function(){console.log('Hey, listening on port %s', port);});
+
+app.set('trust proxy',true);
+
+//sessions
+app.use(session({
+    secret: 'Enter_secret_here',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// body parser middleware
+app.use(express.json());
+// url encoding
+app.use(express.urlencoded({extended:false}));
+    
+// Routes
+app.use('/',require('./server'));
