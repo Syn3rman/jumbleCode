cb40e4f4d12220bbc94d02a7b4c3eedd754470f6
Aditya <aditya10699@gmail.com>
2019-10-12 00:07:24 +0530

    enable cors

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ app.use(session({
     saveUninitialized: true
 }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // body parser middleware
 app.use(express.json());
 // url encoding
