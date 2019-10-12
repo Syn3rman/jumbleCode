89d900447ba2909f36d88f8e794a93375c614700
Aditya <aditya10699@gmail.com>
2019-10-12 08:11:28 +0530

    Added final result

--- server/loganswer.js
+++ server/loganswer.js
@@ -15,11 +15,30 @@ function isLoggedIn(req, res, next){
   }
 }
 
-router.post('/', isLoggedIn, async (req, res)=>{
+router.post('/', async (req, res)=>{
   const db = await dbs.get();
   const crackathon = await db.db('crackathon');
   const round1 = crackathon.collection('round1');
-  
+  const team = req.body.team;
+  const score = req.body.score;
+  round1
+  .insertOne({
+    team,
+    score
+  })
+  .then((result, err)=>{
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }
+    else{
+      res.json({
+        msg: 'Done with the test, please leave',
+        success: true,
+      })
+    }
+  })
+  .catch(e=>console.log(e));
 });
 
 module.exports = router;
